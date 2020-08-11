07d2501ca5bb3fa6f68a364e43fa9628fdc1f80e
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-08-11 19:18:41 +0530

    Change stripe success/ error destination

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,8 +8,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    success_url: `${req.protocol}://${req.get("host")}/all-tours/`,
-    cancel_url: `${req.protocol}://${req.get("host")}/all-tours/${tour.id}`,
+    success_url: `${req.protocol}://${req.get("host")}`,
+    cancel_url: `${req.protocol}://${req.get("host")}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
