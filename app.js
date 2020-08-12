68a083cd851309278a651eb9f1cb6e807e94d5e5
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-08-12 18:52:45 +0530

    Changed the static file serving directory

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use("/api/v1/booking", bookingRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("/", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join("client/build", "index.html"));
   });
 }
